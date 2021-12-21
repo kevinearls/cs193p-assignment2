8c3537cfbcc12bc254a2c8f72fe39aff3442a781
Kevin Earls <kearls@redhat.com>
2021-12-21 17:31:29 +0100

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by Kevin Earls on 21/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment2
+//
+//  Created by Kevin Earls on 21/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
